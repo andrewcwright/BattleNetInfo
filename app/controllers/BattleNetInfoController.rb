5c8f29daced4b20a2c1dd866636bcd6e2103e904
andrewcwright <your_email@whatever.com>
2013-05-24 15:17:20 -0500

    add initial functionality to compare new and old progression

--- app/controllers/BattleNetInfoController.rb
+++ app/controllers/BattleNetInfoController.rb
@@ -46,9 +46,24 @@ class BattleNetInfoController
     battle_net_api = BattleNetAPI.new(character_name, server, progression)
     progression_api_data = battle_net_api.format_progression
     formatted_character = battle_net_api.format_character
-    formatted_server = formatted_character["realm"]
+    formatted_realm = formatted_character["realm"]
+    if new_kills and Character.where(name: character_name, realm: formatted_realm).exists?
+      this_character_id = Character.where(name: character_name, realm: formatted_realm).first.id
+      new_lfr_kills = battle_net_api.num_kills('lfr')
+      old_lfr_kills = Progression.where(name: character_name, character_id: this_character_id).first.lfrBossesKilled
+      new_normal_kills = battle_net_api.num_kills('normal')
+      old_normal_kills = Progression.where(name: character_name, character_id: this_character_id).first.normalBossesKilled
+      new_heroic_kills = battle_net_api.num_kills('heroic')
+      old_heroic_kills = Progression.where(name: character_name, character_id: this_character_id).first.heroicBossesKilled
+      puts "Grats! You have killed #{new_lfr_kills - old_lfr_kills} new bosses in LFR"
+      puts "Grats! You have killed #{new_normal_kills - old_normal_kills} new bosses in normal"
+      puts "Grats! You have killed #{new_heroic_kills - old_heroic_kills} new bosses in heroic"
+      Progression.where(name: character_name, character_id: this_character_id).first.destroy
+    else
+      puts "Sorry! You have not killed any new bosses, QQ"
+    end
     progression_data = {
-      "character_id" => Character.where("name = ? AND realm = ?", character_name, formatted_server ).first.id,
+      "character_id" => Character.where("name = ? AND realm = ?", character_name, formatted_realm).first.id,
       "name" => progression_api_data["name"],
       "lfrBossesKilled" => battle_net_api.num_kills('lfr'),
       "normalBossesKilled" => battle_net_api.num_kills('normal'),
@@ -61,6 +76,29 @@ class BattleNetInfoController
     else
       puts "Failure: #{progression_db.errors.full_messages.join(", ")}"
     end
+  end
+
+  def new_kills
+    character_name = params[:character][:name]
+    server = params[:character][:server]
+    progression = params[:character][:progression]
+    battle_net_api = BattleNetAPI.new(character_name, server, progression)
+    progression_api_data = battle_net_api.format_progression
+    formatted_character = battle_net_api.format_character
+    formatted_realm = formatted_character["realm"]
+    if Character.where(name: character_name, realm: formatted_realm).exists?
+      this_character_id = Character.where(name: character_name, realm: formatted_realm).first.id
+      if Progression.where(name: character_name, character_id: this_character_id).exists?
+        if battle_net_api.num_kills('lfr') > Progression.where(name: character_name, character_id: this_character_id).first.lfrBossesKilled
+          return true
+        elsif battle_net_api.num_kills('normal') > Progression.where(name: character_name, character_id: this_character_id).first.normalBossesKilled
+          return true
+        elsif battle_net_api.num_kills('heroic') > Progression.where(name: character_name, character_id: this_character_id).first.heroicBossesKilled
+          return true
+        end
+      end
+    end
+    return false
   end
 
 end
--- test/integration/test_bni_integration.rb
+++ test/integration/test_bni_integration.rb
@@ -16,4 +16,60 @@ Achievement Points: 8400
     assert_equal(block, `./bni Andose kelthuzad`)
   end
 
+  def test_progression_output
+    block = <<-EOS
+********** Andose **********
+Realm: Kel'Thuzad
+Battlegroup: Nightfall
+Class: Shaman
+Race: Dwarf
+Gender: Male
+Level: 90
+Achievement Points: 8400
+********** Dragon Soul **********
+
+Morchok:
+LFR Kills: 21 - Last Kill: Wed, 09 May 2012 14:28:01 -0500
+Normal Kills: 4 - Last Kill: Wed, 09 May 2012 14:28:01 -0500
+Heroic Kills: 28 - Last Kill: Wed, 15 Aug 2012 20:49:26 -0500
+
+Warlord Zon'ozz:
+LFR Kills: 21 - Last Kill: Wed, 09 May 2012 14:40:28 -0500
+Normal Kills: 13 - Last Kill: Wed, 09 May 2012 14:40:28 -0500
+Heroic Kills: 20 - Last Kill: Wed, 15 Aug 2012 21:14:14 -0500
+
+Yor'sahj the Unsleeping:
+LFR Kills: 22 - Last Kill: Wed, 09 May 2012 14:56:10 -0500
+Normal Kills: 11 - Last Kill: Wed, 09 May 2012 14:56:10 -0500
+Heroic Kills: 22 - Last Kill: Wed, 15 Aug 2012 21:04:31 -0500
+
+Hagara the Stormbinder:
+LFR Kills: 21 - Last Kill: Wed, 09 May 2012 15:08:59 -0500
+Normal Kills: 6 - Last Kill: Mon, 07 May 2012 17:45:43 -0500
+Heroic Kills: 21 - Last Kill: Wed, 15 Aug 2012 21:23:09 -0500
+
+Ultraxion:
+LFR Kills: 15 - Last Kill: Mon, 07 May 2012 17:45:43 -0500
+Normal Kills: 12 - Last Kill: Mon, 07 May 2012 17:45:43 -0500
+Heroic Kills: 21 - Last Kill: Wed, 15 Aug 2012 21:33:22 -0500
+
+Warmaster Blackhorn:
+LFR Kills: 14 - Last Kill: Mon, 07 May 2012 18:03:22 -0500
+Normal Kills: 14 - Last Kill: Mon, 07 May 2012 18:03:22 -0500
+Heroic Kills: 18 - Last Kill: Wed, 15 Aug 2012 21:41:11 -0500
+
+Spine of Deathwing:
+LFR Kills: 12 - Last Kill: Fri, 30 Mar 2012 12:08:42 -0500
+Normal Kills: 15 - Last Kill: Sun, 29 Apr 2012 21:52:40 -0500
+Heroic Kills: 16 - Last Kill: Wed, 15 Aug 2012 21:54:35 -0500
+
+Madness of Deathwing:
+LFR Kills: 12 - Last Kill: Fri, 30 Mar 2012 12:24:54 -0500
+Normal Kills: 15 - Last Kill: Sun, 06 May 2012 23:52:13 -0500
+Heroic Kills: 14 - Last Kill: Wed, 15 Aug 2012 22:22:09 -0500
+
+    EOS
+    assert_equal(block, `./bni Andose kelthuzad "Dragon Soul"`)
+  end
+
 end
