6aa565e287ee761d71cc1a58637676d40e31adcd
andrewcwright <acw462@gmail.com>
2013-05-29 13:51:02 -0500

    change time conversion test

--- test/lib/test_time_converter.rb
+++ test/lib/test_time_converter.rb
@@ -5,7 +5,7 @@ class TestTimeConverter < MiniTest::Unit::TestCase
 
   def test_returns_accurate_time_conversion
     battle_net_api = BattleNetAPI.new("Andose", "kelthuzad")
-    date_time = battle_net_api.time_converter(1369098339)
+    date_time = battle_net_api.time_converter(1369098339000)
     assert_equal "Mon, 20 May 2013 20:05:39 -0500", date_time
   end
 
